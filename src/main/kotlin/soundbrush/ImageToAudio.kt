61772f60fe19961a3d2ed92245d465d5ad35a37d
Juha Komulainen <juha.komulainen@evident.fi>
2018-01-21 21:40:43 +0200

    Use logarithmic frequency scale

--- src/main/kotlin/soundbrush/ImageToAudio.kt
+++ src/main/kotlin/soundbrush/ImageToAudio.kt
@@ -1,26 +1,32 @@
 package soundbrush
 
 import java.awt.image.Raster
+import kotlin.math.log10
+import kotlin.math.pow
 
-fun Raster.toSound(durationSeconds: Double = 3.0, yFactor: Int = 2, maxSpecFreq: Int = 20000): AudioBuffer {
+fun Raster.toSound(durationSeconds: Double = 3.0, minFreq: Int = 0, maxFreq: Int = 20000): AudioBuffer {
+
     val sampleRate = 44100
     val buffer = AudioBuffer(durationSeconds, sampleRate)
     val samplesPerPixel = buffer.size / width
-    val c = maxSpecFreq / height
     val rgb = IntArray(3)
+
+    val maxFreqLog = log10((maxFreq - minFreq).toDouble())
+    val step = maxFreqLog / height
 
     for (i in buffer.indices) {
         val x = (i / samplesPerPixel).coerceAtMost(width - 1)
 
         var sample = 0.0
 
-        for (y in 0 until height step yFactor) {
+        for (y in 0 until height) {
             getPixel(x, y, rgb)
 
             val sum = rgb[0] + rgb[1] + rgb[2]
             val gain = Math.pow(sum * 100 / 765.0, 2.0)
-            val frequency = (c * (height - y + 1))
+            val yy = height - y + 1
 
+            val frequency = minFreq + 10.0.pow(step * yy)
             sample += gain * Math.sin(frequency * Math.PI * 2 * i / sampleRate)
         }
 
--- src/main/kotlin/soundbrush/Main.kt
+++ src/main/kotlin/soundbrush/Main.kt
@@ -2,6 +2,7 @@ package soundbrush
 
 import java.io.File
 import javax.imageio.ImageIO
+import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
     if (args.size != 1) {
@@ -13,9 +14,11 @@ fun main(args: Array<String>) {
     val output = File(input.name.replace(".jpg", ".wav"))
     val image = ImageIO.read(input)
 
-    val buffer = image.raster.toSound(3.0, yFactor = 8)
+    val time = measureTimeMillis {
+        val buffer = image.raster.toSound()
 
-    buffer.writeToWaveFile(output)
+        buffer.writeToWaveFile(output)
+    }
 
-    println("wrote $output")
+    println("wrote $output in $time ms")
 }
